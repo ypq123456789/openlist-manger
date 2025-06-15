f15b59cf6156a1cffaba95aeffa8df9b9ec978b0
ypq123456789 <114487221+ypq123456789@users.noreply.github.com>
2025-06-16 00:09:45 +0800

    Update openlist.sh

--- openlist.sh
+++ openlist.sh
@@ -3,7 +3,7 @@
 #
 # OpenList Interactive Manager Script
 #
-# Version: 1.3.3
+# Version: 1.3.7
 # Last Updated: 2025-06-15
 #
 # Description: 
@@ -27,7 +27,7 @@
 GITHUB_REPO="OpenListTeam/OpenList"
 VERSION_TAG="beta"
 VERSION_FILE="/opt/openlist/.version"
-MANAGER_VERSION="1.3.6"  # 更新管理器版本号
+MANAGER_VERSION="1.3.7"  # 更新管理器版本号
 
 # 颜色配置
 RED_COLOR='\e[1;31m'
